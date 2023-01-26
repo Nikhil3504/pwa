16b59b82bb866f0991f56cbcecc1157040dac86b
Nikhil Mohandas <nikhil_mohandas@thbs.com>
2023-01-26 23:07:29 +0000

    pushing changes

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  myImage: any;
+
+  constructor() {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      saveToGallery: false,
+    });
+
+    this.myImage = image.webPath;
+    console.log(this.myImage);
+  }
+}
